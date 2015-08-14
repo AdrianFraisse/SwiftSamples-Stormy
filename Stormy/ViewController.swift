bb222d203f414d67e5771df71000f7c2675e70f2
Adrian Fraisse <adrian.fraisse@etudiant.mines-nantes.fr>
2015-08-14 22:03:43 +0530

    Initial Commit

---
+++ Stormy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stormy
+//
+//  Created by Adrian Fraisse on 14/08/2015.
+//  Copyright © 2015 Adrian Fraisse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
